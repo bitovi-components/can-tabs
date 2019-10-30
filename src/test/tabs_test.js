c47dbac87abd63b883197c64d13ad42ab9777bd8
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-10-30 16:21:56 +0100

    Landscaper: Migrate to Qunit2

--- src/test/tabs_test.js
+++ src/test/tabs_test.js
@@ -5,22 +5,22 @@ import $ from "jquery";
 
 QUnit.module("can-tabs view model");
 
-QUnit.test("basics", function(){
+QUnit.test("basics", function(assert) {
 	var tabsVM = new CanTabsVM();
 	var panelVM = new CanPanelVM();
 	tabsVM.addPanel(panelVM);
 	
-	equal(panelVM.attr("active"), true, "first panel added is active");
+	assert.equal(panelVM.attr("active"), true, "first panel added is active");
 });
 
 var template = can.stache("<can-tabs><can-panel title='First'>Hello!</can-panel></can-tabs>");
 
 QUnit.module("can-tabs component",{
-	setup: function(){
+	beforeEach: function(assert) {
 		$("#qunit-fixture").append(template());
 	}
 });
 
-QUnit.test("basics", function(){
-	QUnit.equal( $.trim( $("can-tabs ul li").text() ),"First", "has text");
+QUnit.test("basics", function(assert) {
+	assert.equal( $.trim( $("can-tabs ul li").text() ),"First", "has text");
 });
